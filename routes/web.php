15d793b86b386b102e6f67d91b8c6b356e8af7d0
sokkean bunchhun <sokkeanbunchhun1@gmail.com>
2025-06-17 17:19:05 +0700

    modify

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/test-log', function () {
+    Log::info('✅ Logging works on Railway using stderr');
+    return 'Check Railway → Logs tab!';
 });
