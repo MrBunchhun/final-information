6b3b97f9332743309e97c2fb6f8e78e6e7b0f596
sokkean bunchhun <sokkeanbunchhun1@gmail.com>
2025-06-29 14:37:17 +0700

    style for backpack

---
+++ resources/views/vendor/backpack/base/inc/head.blade.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ config('backpack.base.project_name') ?? 'Backpack' }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- ✅ Vite assets --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{-- Backpack default meta --}}
+    @stack('before_styles')
+    @stack('after_styles')
+</head>
